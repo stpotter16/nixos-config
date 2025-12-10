914736035e1c5e32489579cfce6a516d57aa8c37
Sam Potter <spotter1642@gmail.com>
2025-12-10 09:10:24 -0500

    Back to defining litestream settings in nix file

--- nixos/litestream.nix
+++ nixos/litestream.nix
@@ -2,5 +2,25 @@
   services.litestream = {
     enable = true;
     environmentFile = "/run/secrets/litestream";
+    settings = {
+      dbs = [
+        {
+          path = "/var/lib/biodata/biodata.sqlite";
+          replicas = [
+            {
+              type = "s3";
+              bucket = "biotrak";
+              path = "db";
+              endpoint = "TODO";
+              force-path-style = true;
+              retention = "168h";
+              snapshot-interval = "24h";
+              validation-interval = "72h";
+              sync-interval = "15m";
+            }
+          ];
+        }
+      ];
+    };
   };
 }
