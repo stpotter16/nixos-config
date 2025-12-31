dcafa681dde2266feb60ee59f2de8c5292f12967
Sam Potter <spotter1642@gmail.com>
2025-12-30 20:22:48 -0500

    Add encoding to caddy configurations

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -2,23 +2,25 @@
   services.caddy = {
     enable = true;
     virtualHosts."leadreacherai.com".extraConfig = ''
-      encode gzip
+      encode zstd gzip
       root * /var/www/html/leadreacherai
       file_server {
         hide .git LICENSE
       }
     '';
     virtualHosts."www.leadreacherai.com".extraConfig = ''
-      encode gzip
+      encode zstd gzip
       root * /var/www/html/leadreacherai
       file_server {
         hide .git LICENSE
       }
     '';
     virtualHosts."biotrak.app".extraConfig = ''
+      encode zstd gzip
       reverse_proxy :8080
     '';
     virtualHosts."biodata.stpotter.dev".extraConfig = ''
+      encode zstd gzip
       reverse_proxy :8080
     '';
   };
